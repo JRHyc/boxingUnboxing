8a3dd9b0341dbc79bdf96d147374227a69d51bcc
Jack Hyc <Jacek.Hyc@gmail.com>
2018-03-05 18:24:23 -0600

    Finished assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace boxingUnboxing
 {
@@ -7,6 +8,30 @@ namespace boxingUnboxing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            List<object> empty = new List<object>();
+            empty.Add(7);
+            empty.Add(28);
+            empty.Add(-1);
+            empty.Add(true);
+            empty.Add("chair");
+            int sum = new int();
+            foreach (var entry in empty)
+            {
+                if (entry is int)
+                {
+                    sum += (int)entry;
+                }
+                Console.WriteLine(entry);    
+            }            
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
         }
     }
 }
